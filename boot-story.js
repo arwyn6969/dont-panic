df7b7cb4eea43bacaea19fa95239129738ae77be
ARWYN <63277321+arwyn6969@users.noreply.github.com>
2026-09-22 14:13:32 +0100

    Fetch the Infocom z3 from historicalsource if it is not embedded

---
+++ boot-story.js
@@ -0,0 +1,5 @@
+window.STORY_FETCH_URLS = [
+  "story/s4.z3",
+  "https://cdn.jsdelivr.net/gh/historicalsource/hitchhikersguide@master/COMPILED/s4.z3",
+  "https://raw.githubusercontent.com/historicalsource/hitchhikersguide/master/COMPILED/s4.z3"
+];
